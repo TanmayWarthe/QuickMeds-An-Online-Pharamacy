4e9995e40b06a10e26e2f5f004770857e7fa51a2
Rhushabh Gumte <rhushabhgumte0485@gmail.com>
2025-04-17 16:30:08 +0530

    makechange

--- quickmeds/settings.py
+++ quickmeds/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # For production, enable this
 SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=False, cast=bool)
